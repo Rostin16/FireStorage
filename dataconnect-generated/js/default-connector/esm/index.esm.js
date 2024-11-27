9aec6c9432c39b46f2344a7d49f5006fb867d2eb
rostin16 <roshnijp16@gmail.com>
2024-11-27 23:46:35 +0530

    deploy

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'FireStorage-PR',
+  location: 'us-central1'
+};
+
